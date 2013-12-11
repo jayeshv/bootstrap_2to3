9de7d81df922a8b181f4d3700d3e03a4a1dbe74a
Jayesh <jayesh.mail@gmail.com>
2013-12-11 13:17:38 +0530

    packaging changes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='bootstrap_2to_3',
-    version='0.1',
+    name='bootstrap_2to3',
+    version='0.1.1',
     author='Jayesh',
     author_email='jayesh@jayeshv.info',
-    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    packages=['bootstrap_2to3'],
     url='https://github.com/jayeshv/bootstrap_2to3',
-    license='LICENSE.txt',
+    license='BSD licence',
     description='Script to migrate bootstrap 2 templates to bootstrap3.',
-    long_description=open('README.txt').read(),
+    #long_description=open('README.md').read(),
 )
